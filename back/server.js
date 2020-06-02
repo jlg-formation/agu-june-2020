8820d92745578eea8738b553bed0bd67630edc99
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-02 10:19:41 +0200

    add middleware log

--- back/server.js
+++ back/server.js
@@ -1,3 +1,17 @@
 console.log("starting server");
 
 const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+    console.log('req.url: ', req.url);
+    next();
+});
+
+app.use((req, res, next) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
