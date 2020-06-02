3ba4c8943f9da0c1efa89cf71872f83756ea9a10
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-02 10:28:07 +0200

    express static ok

--- back/server.js
+++ back/server.js
@@ -1,16 +1,18 @@
 console.log("starting server");
 
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    console.log('req.url: ', req.url);
-    next();
+  console.log("req.url: ", req.url);
+  next();
 });
 
-app.use((req, res, next) => res.send("Hello World!"));
+app.use(express.static("./public"));
+app.use(serveIndex("./public"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
