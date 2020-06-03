5421326d6c3d7221de890631b62591715d35a553
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-03 11:21:28 +0200

    added font awesome

--- front/src/app/stock/list/list.component.ts
+++ front/src/app/stock/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  faCoffee = faCoffee;
   constructor() {}
 
   ngOnInit(): void {}
--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { StockRoutingModule } from './stock-routing.module';
 import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
 
-
 @NgModule({
   declarations: [ListComponent, CreateComponent],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  imports: [CommonModule, StockRoutingModule, FontAwesomeModule],
 })
-export class StockModule { }
+export class StockModule {}
