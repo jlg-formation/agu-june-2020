cea723811a131318e0b366c412c1f46ba858b58d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-03 16:43:26 +0200

    maintain selected article list

--- front/src/app/stock/list/list.component.ts
+++ front/src/app/stock/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faRedo, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-list',
@@ -11,11 +12,27 @@ export class ListComponent implements OnInit {
   faRedo = faRedo;
   faPlus = faPlus;
   faTrashAlt = faTrashAlt;
+
+  selectedArticles: Article[] = [];
+
   constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
 
-  select() {
-    console.log('select');
+  select(event: MouseEvent, article: Article) {
+    console.log('select', event);
+    const cell = event.target as HTMLElement;
+    const row = cell.parentElement;
+    console.log('row: ', row);
+    if (row.classList.contains('selected')) {
+      row.classList.remove('selected');
+      const index = this.selectedArticles.findIndex((a) => a === article);
+      this.selectedArticles.splice(index, 1);
+      console.log('this.selectedArticles: ', this.selectedArticles);
+      return;
+    }
+    row.classList.add('selected');
+    this.selectedArticles.push(article);
+    console.log('this.selectedArticles: ', this.selectedArticles);
   }
 }
