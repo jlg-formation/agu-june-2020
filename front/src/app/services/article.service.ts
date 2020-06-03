2c11685bf7585d97b3f16c00a42ae7cd2c5b48f9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-03 16:18:50 +0200

    using localstorage

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,19 +5,31 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Marteau', price: 4.56, qty: 10 },
-    { name: 'Scie', price: 7.86, qty: 13 },
-    { name: 'Pince', price: 3.0, qty: 15 },
-    { name: 'Clé à molette en prix disounted', price: 3.0, qty: 15 },
-  ];
-
+  articles = this.restore();
   constructor() {
-    console.log('coucouc je cree le article service');
+    console.log('coucou je cree le article service');
+  }
+
+  restore(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 2.34, qty: 123 },
+        { name: 'Marteau', price: 4.56, qty: 10 },
+        { name: 'Scie', price: 7.86, qty: 13 },
+        { name: 'Pince', price: 3.0, qty: 15 },
+        { name: 'Clé à molette en prix disounted', price: 3.0, qty: 15 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
+
+  save() {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
   }
 
   add(article: Article) {
     this.articles.push(article);
+    this.save();
   }
 }
