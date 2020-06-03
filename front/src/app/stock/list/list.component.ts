71f067d0d934cb209569b61d015da41d6dd5d05b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-03 10:54:29 +0200

    added css for list

--- front/src/app/stock/list/list.component.ts
+++ front/src/app/stock/list/list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
