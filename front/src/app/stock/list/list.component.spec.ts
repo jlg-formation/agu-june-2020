70d998f7ab9213143942627cafd5a91bd0173971
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-04 16:18:17 +0200

    increased coverage

--- front/src/app/stock/list/list.component.spec.ts
+++ front/src/app/stock/list/list.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
 import { WidgetModule } from 'src/app/widget/widget.module';
+import { ArticleService } from 'src/app/services/article.service';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -22,5 +23,35 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should delete', () => {
+    const articleService: ArticleService = TestBed.inject(ArticleService);
+    const article = { id: 'a2', name: 'Tournevis Cruciforme', price: 2.34, qty: 123 };
+    articleService.articles = [article];
+    articleService.save();
+    fixture.detectChanges();
+    component.selectedArticles = [article];
+    component.delete();
+    expect(articleService.articles.length === 0).toBeTrue();
+  });
+
+  it('should select', () => {
+    const articleService: ArticleService = TestBed.inject(ArticleService);
+    const article = { id: 'a2', name: 'Tournevis', price: 2.34, qty: 123 };
+    articleService.articles = [article];
+    articleService.save();
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const cell: HTMLElement = compiled.querySelector('tbody tr td');
+    // select
+    cell.click();
+    expect(component.selectedArticles.length === 1).toBeTrue();
+    const row: HTMLElement = compiled.querySelector('tbody tr');
+    expect(row.classList.contains('selected')).toBeTrue();
+    // deselect
+    cell.click();
+    expect(component.selectedArticles.length === 0).toBeTrue();
+    expect(row.classList.contains('selected')).toBeFalse();
   });
 });
--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,35 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, async, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { AutofocusDirective } from './autofocus.directive';
 
+@Component({
+  template: `<input type="text" appAutofocus />`,
+})
+class FakeComponent {}
+
 describe('AutofocusDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new AutofocusDirective();
-  //   expect(directive).toBeTruthy();
-  // });
+  let component: FakeComponent;
+  let fixture: ComponentFixture<FakeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FakeComponent, AutofocusDirective],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FakeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should put the focus on the input', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const hasFocus = document.activeElement === compiled.querySelector('input');
+    expect(hasFocus).toBeTrue();
+  });
 });
