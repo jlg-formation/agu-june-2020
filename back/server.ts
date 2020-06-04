b6bc666064c3f9e895989f68c2801c5a84ed7ba1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-04 12:16:29 +0200

    add fake latency to the server

--- back/server.ts
+++ back/server.ts
@@ -12,6 +12,12 @@ const port = 3000;
 app.use(cors());
 
 app.use((req, res, next) => {
+  setTimeout(() => {
+    next();
+  }, 1000);
+});
+
+app.use((req, res, next) => {
   console.log("req.url: ", req.url);
   next();
 });
