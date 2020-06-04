3e8c39076dfd8cc1bc7c9d57fbf48c88daf95623
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-04 11:59:10 +0200

    added delete on back office

--- back/ws.ts
+++ back/ws.ts
@@ -3,11 +3,11 @@ import { Article } from "../front/src/app/interfaces/article";
 const app = express.Router();
 
 const articles: Article[] = [
-  { name: "Tournevis", price: 2.34, qty: 123 },
-  { name: "Marteau", price: 4.56, qty: 10 },
-  { name: "Scie", price: 7.86, qty: 13 },
-  { name: "Pince", price: 3.0, qty: 15 },
-  { name: "Clé à molette en prix disounted", price: 3.0, qty: 15 },
+  { id: "a1", name: "Tournevis", price: 2.34, qty: 123 },
+  { id: "a2", name: "Marteau", price: 4.56, qty: 10 },
+  { id: "a3", name: "Scie", price: 7.86, qty: 13 },
+  { id: "a4", name: "Pince", price: 3.0, qty: 15 },
+  { id: "a5", name: "Clé à molette en prix disounted", price: 3.0, qty: 15 },
 ];
 
 app.use(express.json());
@@ -21,6 +21,13 @@ app.post("/articles", (req, res) => {
   console.log("article: ", article);
   articles.push(article);
   res.status(201).json(article);
+});
+
+app.delete("/articles/:myId", (req, res) => {
+  const id = req.params.myId;
+  const index = articles.findIndex((a) => a.id === id);
+  articles.splice(index, 1);
+  res.status(204).end();
 });
 
 export const ws = app;
