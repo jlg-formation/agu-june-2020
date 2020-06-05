902834f4e95d4c68797c531c9552720aec217be7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 14:43:48 +0200

    example of new observable

--- front/src/app/widget/chrono/chrono.component.ts
+++ front/src/app/widget/chrono/chrono.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chrono',
   templateUrl: './chrono.component.html',
-  styleUrls: ['./chrono.component.scss']
+  styleUrls: ['./chrono.component.scss'],
 })
 export class ChronoComponent implements OnInit {
+  counter = 90;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    // observable qui compte jusqu'à 11 et qui part en erreur aussitot.
+    const myInterval = new Observable<number>((observer) => {
+      const obj = {
+        counter: 0,
+      };
+      observer.next(obj.counter);
+      obj.counter++;
+      const timeout = setInterval(() => {
+        console.log('interval going', obj.counter);
+        if (obj.counter > 10) {
+          observer.error('oui je suis une erreur');
+          clearInterval(timeout);
+        }
+        observer.next(obj.counter);
+        obj.counter++;
+      }, 1000);
 
+      // si on interrompt l'observable il se nettoie tout seul
+      observer.add(() => {
+        clearInterval(timeout);
+      });
+    });
+    const subsbription = myInterval.subscribe({
+      next: (val) => {
+        this.counter = val;
+        if (val > 5) {
+          subsbription.unsubscribe();
+        }
+      },
+      error: (err) => (this.counter = err),
+    });
+  }
 }
