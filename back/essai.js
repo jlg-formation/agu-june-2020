81139ed698756716b5a09eff8b5ae8122af87dbb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 10:09:50 +0200

    essai.js is now asynchrone

--- back/essai.js
+++ back/essai.js
@@ -1,7 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.appendFile(path.resolve(__dirname, "./toto.txt"), "hello jlg\n",);
-fs.appendFileSync(path.resolve(__dirname, "./toto.txt"), "hello jlg\n");
-fs.appendFileSync(path.resolve(__dirname, "./toto.txt"), "hello jlg\n");
-fs.appendFileSync(path.resolve(__dirname, "./toto.txt"), "hello jlg\n");
+// tache 1
+fs.appendFile(path.resolve(__dirname, "./toto.txt"), "hello jlg\n", (err) => {
+  // tache 2
+  fs.appendFile(path.resolve(__dirname, "./toto.txt"), "hello jlg\n", (err) => {
+    // tache 3
+    fs.appendFile(
+      path.resolve(__dirname, "./toto.txt"),
+      "hello jlg\n",
+      (err) => {
+        // tache 4
+        fs.appendFile(
+          path.resolve(__dirname, "./toto.txt"),
+          "hello jlg\n",
+          (err) => {
+            // tache 5
+          }
+        );
+      }
+    );
+  });
+});
