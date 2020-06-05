798c65afd0ecbbc3924444db0b2d438c2c1477fc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 11:25:01 +0200

    bug timeout 504

--- back/ws.ts
+++ back/ws.ts
@@ -4,16 +4,16 @@ import fs from "fs";
 import path from "path";
 const app = express.Router();
 
-const filename = path.resolve(__dirname, "./data.json");
+const filename = path.resolve(__dirname, "./data.db");
 
 const articles: Article[] = JSON.parse(
-  fs.readFileSync(filename, { encoding: "utf8" })
+  fs.readFileSync(filename, { encoding: "utf-8" })
 );
 
 app.use(express.json());
 
 app.get("/articles", (req, res) => {
-  fs.readFile(filename, { encoding: "utf8" }, (err, result) => {
+  fs.readFile(filename, { encoding: "utf-8" }, (err, result) => {
     if (err) {
       res.status(500).end();
       return;
@@ -23,30 +23,45 @@ app.get("/articles", (req, res) => {
   });
 });
 
-app.post("/articles", async (req, res) => {
-  const article = req.body;
-  console.log("article: ", article);
-  articles.push(article);
-  await fs.promises.writeFile(filename, JSON.stringify(articles, undefined, 2));
-  res.status(201).json(article);
+app.post("/articles", (req, res) => {
+  (async () => {
+    const article = req.body;
+    console.log("article: ", article);
+    articles.push(article);
+    await fs.promises.writeFile(
+      filename,
+      JSON.stringify(articles, undefined, 2)
+    );
+    res.status(201).json(article);
+  })();
 });
 
-app.delete("/articles/:myId", async (req, res) => {
-  const id = req.params.myId;
-  const index = articles.findIndex((a) => a.id === id);
-  articles.splice(index, 1);
-  await fs.promises.writeFile(filename, JSON.stringify(articles, undefined, 2));
-  res.status(204).end();
-});
-
-app.delete("/bulk/articles", async (req, res) => {
-  const ids = req.body;
-  for (const id of ids) {
+app.delete("/articles/:myId", (req, res) => {
+  (async () => {
+    const id = req.params.myId;
     const index = articles.findIndex((a) => a.id === id);
     articles.splice(index, 1);
-  }
-  await fs.promises.writeFile(filename, JSON.stringify(articles, undefined, 2));
-  res.status(204).end();
+    await fs.promises.writeFile(
+      filename,
+      JSON.stringify(articles, undefined, 2)
+    );
+    res.status(204).end();
+  })();
+});
+
+app.delete("/bulk/articles", (req, res) => {
+  (async () => {
+    const ids = req.body;
+    for (const id of ids) {
+      const index = articles.findIndex((a) => a.id === id);
+      articles.splice(index, 1);
+    }
+    await fs.promises.writeFile(
+      filename,
+      JSON.stringify(articles, undefined, 2)
+    );
+    res.status(204).end();
+  })();
 });
 
 export const ws = app;
