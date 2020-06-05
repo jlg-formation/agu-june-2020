f342dff2f6236626f3fec60c7bea6333b712c64f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 10:19:54 +0200

    pass to promise

--- back/essai.js
+++ back/essai.js
@@ -1,24 +1,40 @@
 const fs = require("fs");
 const path = require("path");
 
-// tache 1
-fs.appendFile(path.resolve(__dirname, "./toto.txt"), "hello jlg\n", (err) => {
-  // tache 2
-  fs.appendFile(path.resolve(__dirname, "./toto.txt"), "hello jlg\n", (err) => {
-    // tache 3
-    fs.appendFile(
-      path.resolve(__dirname, "./toto.txt"),
-      "hello jlg\n",
-      (err) => {
-        // tache 4
-        fs.appendFile(
-          path.resolve(__dirname, "./toto.txt"),
-          "hello jlg\n",
-          (err) => {
-            // tache 5
-          }
-        );
+function appendFilePromise(file, str) {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(file, str, (err, result) => {
+      if (err) {
+        return reject(err);
       }
-    );
+      resolve(result);
+    });
   });
-});
+}
+
+// tache 1
+appendFilePromise(path.resolve(__dirname, "./toto.txt"), "hello jlg\n")
+  .then((result) => {
+      // tache 2
+    return appendFilePromise(
+      path.resolve(__dirname, "./toto.txt"),
+      "hello jlg\n"
+    );
+  })
+  .then((result) => {
+      // tache 3
+    return appendFilePromise(
+      path.resolve(__dirname, "./toto.txt"),
+      "hello jlg\n"
+    );
+  })
+  .then((result) => {
+      // tache 4
+    return appendFilePromise(
+      path.resolve(__dirname, "./toto.txt"),
+      "hello jlg\n"
+    );
+  })
+  .catch((err) => {
+    console.log("err: ", err);
+  });
