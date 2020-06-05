e05054b8d13c68ed4f0d2f373fc98f8fa4cd837a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 15:22:36 +0200

    chrono per page

--- front/src/app/widget/chrono/chrono.component.ts
+++ front/src/app/widget/chrono/chrono.component.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import {
+  interval,
+  Observable,
+  Subject,
+  timer,
+  BehaviorSubject,
+  Subscription,
+} from 'rxjs';
+import { map, startWith, subscribeOn } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-chrono',
   templateUrl: './chrono.component.html',
   styleUrls: ['./chrono.component.scss'],
 })
-export class ChronoComponent implements OnInit {
-  counter = 90;
+export class ChronoComponent {
+  counter$ = new BehaviorSubject(0);
+  pageCounter$ = new BehaviorSubject(0);
+  subscription: Subscription;
+  constructor(private router: Router) {
+    interval(1000)
+      .pipe(map((x) => x + 1))
+      .subscribe(this.counter$);
 
-  constructor() {}
-
-  ngOnInit(): void {
-    interval(1000).pipe(
-      map((x) => x + 1),
-      startWith(0)
-    ).subscribe((val) => (this.counter = val));
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+        this.subscription = interval(1000)
+          .pipe(map((x) => x + 1))
+          .subscribe(this.pageCounter$);
+      }
+    });
   }
 }
