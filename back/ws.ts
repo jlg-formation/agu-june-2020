e7c9a4ba1b83950a4b2821df5472baaa42be88e3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 11:02:12 +0200

    added file write

--- back/ws.ts
+++ back/ws.ts
@@ -4,47 +4,48 @@ import fs from "fs";
 import path from "path";
 const app = express.Router();
 
+const filename = path.resolve(__dirname, "./data.json");
+
 const articles: Article[] = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "./data.json"), { encoding: "utf8" })
+  fs.readFileSync(filename, { encoding: "utf8" })
 );
 
 app.use(express.json());
 
 app.get("/articles", (req, res) => {
-  fs.readFile(
-    path.resolve(__dirname, "./data.json"),
-    { encoding: "utf8" },
-    (err, result) => {
-      if (err) {
-        res.status(500).end();
-        return;
-      }
-      const articles = JSON.parse(result);
-      res.json(articles);
+  fs.readFile(filename, { encoding: "utf8" }, (err, result) => {
+    if (err) {
+      res.status(500).end();
+      return;
     }
-  );
+    const articles = JSON.parse(result);
+    res.json(articles);
+  });
 });
 
-app.post("/articles", (req, res) => {
+app.post("/articles", async (req, res) => {
   const article = req.body;
   console.log("article: ", article);
   articles.push(article);
+  await fs.promises.writeFile(filename, JSON.stringify(articles, undefined, 2));
   res.status(201).json(article);
 });
 
-app.delete("/articles/:myId", (req, res) => {
+app.delete("/articles/:myId", async (req, res) => {
   const id = req.params.myId;
   const index = articles.findIndex((a) => a.id === id);
   articles.splice(index, 1);
+  await fs.promises.writeFile(filename, JSON.stringify(articles, undefined, 2));
   res.status(204).end();
 });
 
-app.delete("/bulk/articles", (req, res) => {
+app.delete("/bulk/articles", async (req, res) => {
   const ids = req.body;
   for (const id of ids) {
     const index = articles.findIndex((a) => a.id === id);
     articles.splice(index, 1);
   }
+  await fs.promises.writeFile(filename, JSON.stringify(articles, undefined, 2));
   res.status(204).end();
 });
 
