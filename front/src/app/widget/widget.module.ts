5f7224f7316b022f85dd5852190e3f3e3f840d8d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-05 14:15:47 +0200

    added chrono component

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
 import { EllipsisPipe } from './ellipsis.pipe';
+import { ChronoComponent } from './chrono/chrono.component';
 
 
 
 @NgModule({
-  declarations: [AutofocusDirective, EllipsisPipe],
+  declarations: [AutofocusDirective, EllipsisPipe, ChronoComponent],
   imports: [
     CommonModule
   ],
-  exports: [AutofocusDirective, EllipsisPipe]
+  exports: [AutofocusDirective, EllipsisPipe, ChronoComponent]
 })
 export class WidgetModule { }
